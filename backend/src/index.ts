008df1a9272d5483cf29c2c370aebc7f8049344f
khaledakel <khaled-akel@outlook.com>
2024-08-09 22:26:33 +0200

    fix

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -13,7 +13,7 @@ const app = express();
 // Middlewares
 app.use(cors(
     {
-        origin: process.env.FRONTEND_URL as string || 'http://localhost:5173',
+        origin: 'https://booking-com-mern.onrender.com/' || 'http://localhost:5173',
         credentials: true
     }
 ));
