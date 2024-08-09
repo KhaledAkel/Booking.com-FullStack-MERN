7ca75ddf81da1a22b5de447a9718d44b1d1d6aa1
khaledakel <khaled-akel@outlook.com>
2024-08-09 20:04:27 +0200

    register test

--- e2e-tests/tests/auth.spec.ts
+++ e2e-tests/tests/auth.spec.ts
@@ -1,18 +1,17 @@
 import { test, expect } from '@playwright/test';
 
-test('has title', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
+const UI_URL = 'http://localhost:5173';
 
-  // Expect a title "to contain" a substring.
-  await expect(page).toHaveTitle(/Playwright/);
-});
+test('allow to regiter', async ({ page }) => {
 
-test('get started link', async ({ page }) => {
-  await page.goto('https://playwright.dev/');
-
-  // Click the get started link.
-  await page.getByRole('link', { name: 'Get started' }).click();
-
-  // Expects page to have a heading with the name of Installation.
-  await expect(page.getByRole('heading', { name: 'Installation' })).toBeVisible();
+  await page.goto(UI_URL);
+  await page.getByRole('link', { name: 'Sign up' }).click();
+  await page.fill('input[name="first_name"]', 'test_first_name');
+  await page.fill('input[name="last_name"]', 'test_last_name');
+  await page.fill('input[name="email"]', `test@${Date.now()}test.com`);
+  await page.fill('input[name="password"]', 'test_12345678');
+  await page.fill('input[name="confirm_password"]', 'test_12345678');
+  await page.click('button[type="submit"]');
+  await page.waitForNavigation();
+  await expect(page.url()).toBe(`${UI_URL}/`);
 });
